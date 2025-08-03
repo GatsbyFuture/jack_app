bc8765889a21cc93c96f9602c8eefe767dbd5ce6
Jack <raxmatoveldor37@gmail.com>
2025-08-03 20:00:17 +0500

    Login and Register pages done!

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,6 +1,26 @@
+import {useState} from 'react';
+import {logo} from '../constants';
+import {Input} from '../ui';
+
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <div>Login</div>
+        <div className="text-center">
+            <main className="form-signin w-25 m-auto">
+                <form>
+                    <img className="mb-4" src={logo} alt="" width="200"
+                         height="57"/>
+                    <h1 className="h3 mb-3 fw-normal">Please register</h1>
+
+                    <Input label={'Email address'} state={email} setState={setEmail}/>
+                    <Input label={'Password'} state={password} setState={setPassword}/>
+
+                    <button className="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+                </form>
+            </main>
+        </div>
     );
 }
 
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,6 +1,28 @@
+import {logo} from '../constants';
+import {Input} from '../ui';
+import {useState} from 'react';
+
 const Register = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <div>Register</div>
+        <div className="text-center">
+            <main className="form-signin w-25 m-auto">
+                <form>
+                    <img className="mb-4" src={logo} alt="" width="200"
+                         height="57"/>
+                    <h1 className="h3 mb-3 fw-normal">Please register</h1>
+
+                    <Input label={'Username'} state={name} setState={setName}/>
+                    <Input label={'Email address'} state={email} setState={setEmail}/>
+                    <Input label={'Password'} state={password} setState={setPassword}/>
+
+                    <button className="w-100 btn btn-lg btn-primary" type="submit">Register</button>
+                </form>
+            </main>
+        </div>
     );
 }
 
